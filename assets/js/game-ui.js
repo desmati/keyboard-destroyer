5cb5c29380fbedfeaf1a254ac35cce721109e5d4
Erik Friis <erik.friis@hyperisland.sem>
2022-11-22 12:51:47 +0100

    added win dialog

--- assets/js/game-ui.js
+++ assets/js/game-ui.js
@@ -1,71 +1,70 @@
 class GameUI {
-    constructor() {
-        this.gameboardkey = document.getElementById('gameboard__key');
-        this.yourSpeedElement = document.getElementById('gameboard__your-speed');
+  constructor() {
+    this.gameboardkey = document.getElementById("gameboard__key");
+    this.yourSpeedElement = document.getElementById("gameboard__your-speed");
 
-        this.KeyList = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
-        this.MaxKeyPress = 0;
+    this.KeyList = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+    this.MaxKeyPress = 0;
 
-        this.CurrentKey = "";
+    this.CurrentKey = "";
 
-        this.CurrentKeyPressesCount = 0;
-        this.RemainedKeyStrokes = 1;
-        this.KeyPressesCount = 0;
-    }
+    this.CurrentKeyPressesCount = 0;
+    this.RemainedKeyStrokes = 1;
+    this.KeyPressesCount = 0;
+  }
 
-    HandleUserInput(container, timer, increaseAmount, decreaseAmount) {
-        window.addEventListener('keyup', (e) => {
+  HandleUserInput(container, timer, increaseAmount, decreaseAmount) {
+    window.addEventListener("keyup", (e) => {
+      if (!container.isFreezed) {
+        var key = e.key.toUpperCase();
 
-            if (!container.isFreezed) {
-                var key = e.key.toUpperCase();
-
-
-                if (this.CurrentKey === key) {
-                    container.Increase(increaseAmount);
-                    this.RemainedKeyStrokes--;
-                    this.KeyPressesCount++;
-                } else {
-                    container.Decrease(decreaseAmount);
-                }
-
-                if (this.RemainedKeyStrokes === 0) {
-                    // this.MaxKeyPress = Math.floor(Math.random() * 3);
-                    this.RandomChangeInput(this.MaxKeyPress);
-                    this.DisplayKey();
-                }
-            }
-        });
-    }
-
-    RandomChangeInput(maxKeyPress) {
-        this.MaxKeyPress = maxKeyPress;
-        this.RemainedKeyStrokes = maxKeyPress;
-
-        let random = Math.floor(Math.random() * this.KeyList.length);
-        this.CurrentKey = this.KeyList[random];
-
-
-    }
-
-    DisplayKey() {
-        this.gameboardkey.innerHTML = this.CurrentKey;
-    }
-
-    DisplayKeySpeed(interval, container, timer) {
-        if (this.keySpeedInterval) {
-            clearInterval(this.keySpeedInterval);
+        if (this.CurrentKey === key) {
+          container.Increase(increaseAmount);
+          this.RemainedKeyStrokes--;
+          this.KeyPressesCount++;
+        } else {
+          container.Decrease(decreaseAmount);
         }
 
-        this.keySpeedInterval = setInterval(() => {
-            if (!container.isFreezed) {
-                this.yourSpeedElement.innerHTML = (this.KeyPressesCount / timer.HowMuchPassed * 1000).toFixed(2);
-            }
-        }, interval);
+        if (this.RemainedKeyStrokes === 0) {
+          // this.MaxKeyPress = Math.floor(Math.random() * 3);
+          this.RandomChangeInput(this.MaxKeyPress);
+          this.DisplayKey();
+        }
+      }
+    });
+  }
+
+  RandomChangeInput(maxKeyPress) {
+    this.MaxKeyPress = maxKeyPress;
+    this.RemainedKeyStrokes = maxKeyPress;
+
+    let random = Math.floor(Math.random() * this.KeyList.length);
+    this.CurrentKey = this.KeyList[random];
+  }
+
+  DisplayKey() {
+    this.gameboardkey.innerHTML = this.CurrentKey;
+  }
+
+  DisplayKeySpeed(interval, container, timer) {
+    if (this.keySpeedInterval) {
+      clearInterval(this.keySpeedInterval);
     }
 
-    DisplayStart() {
-        let dialog = new Dialog();
-        dialog.Display(`
+    this.keySpeedInterval = setInterval(() => {
+      if (!container.isFreezed) {
+        this.yourSpeedElement.innerHTML = (
+          (this.KeyPressesCount / timer.HowMuchPassed) *
+          1000
+        ).toFixed(2);
+      }
+    }, interval);
+  }
+
+  DisplayStart() {
+    let dialog = new Dialog();
+    dialog.Display(`
             <div id="start-game-intro">
                 Hey welcome to the game stupid
                 rules rules ruless ruuuuleeesss rules booooring ruuuiules
@@ -73,15 +72,15 @@ class GameUI {
             <button id="start-game-btn">Start Game</button>
         `);
 
-        let startButtonElement = document.getElementById('start-game-btn');
-        startButtonElement.addEventListener('click', () => {
-            dialog.Close();
-        });
-    }
+    let startButtonElement = document.getElementById("start-game-btn");
+    startButtonElement.addEventListener("click", () => {
+      dialog.Close();
+    });
+  }
 
-    DisplayLossDialog(game) {
-        let dialog = new Dialog();
-        dialog.Display(`
+  DisplayLossDialog(game) {
+    let dialog = new Dialog();
+    dialog.Display(`
             <div id="start-game-intro">
                 <h2>You Lost</h2>
             </div>
@@ -89,13 +88,40 @@ class GameUI {
             <button id="restart-game-btn">Restart Game</button>
         `);
 
-        let restartButtonElement = document.getElementById('restart-game-btn');
-        restartButtonElement.addEventListener('click', () => {
-            dialog.Close();
-            game.Restart();
-        });
+    let restartButtonElement = document.getElementById("restart-game-btn");
+    restartButtonElement.addEventListener("click", () => {
+      dialog.Close();
+      game.Restart();
+    });
 
-        let scoreboardContentElement = document.getElementById('score-board-content');
-        scoreboardContentElement.innerHTML = document.getElementById('gameboard__scoreboard').innerHTML;
-    }
+    let scoreboardContentElement = document.getElementById(
+      "score-board-content"
+    );
+    scoreboardContentElement.innerHTML = document.getElementById(
+      "gameboard__scoreboard"
+    ).innerHTML;
+  }
+  DisplayWinDialog(game) {
+    let dialog = new Dialog();
+    dialog.Display(`
+            <div id="start-game-intro">
+                <h2>You Win</h2>
+            </div>
+            <div id="score-board-content"></div>
+            <button id="restart-game-btn">Restart Game</button>
+        `);
+
+    let restartButtonElement = document.getElementById("restart-game-btn");
+    restartButtonElement.addEventListener("click", () => {
+      dialog.Close();
+      game.Restart();
+    });
+
+    let scoreboardContentElement = document.getElementById(
+      "score-board-content"
+    );
+    scoreboardContentElement.innerHTML = document.getElementById(
+      "gameboard__scoreboard"
+    ).innerHTML;
+  }
 }
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,41 +1,43 @@
 class Game {
-    constructor() {
+  constructor() {}
 
+  Initialize(timer, ui, container) {
+    this.timer = timer;
+    this.ui = ui;
+    this.container = container;
+
+    this.Restart();
+  }
+
+  StartGame() {
+    if (this.started) {
+      return;
     }
 
-    Initialize(timer, ui, container) {
-        this.timer = timer;
-        this.ui = ui;
-        this.container = container;
+    this.timer.ResetTimer(config.startTime);
+    this.timer.StartTimer();
+    this.timer.OnTimeUpEvent(() => {
+      ui.DisplayLossDialog(this);
+      this.container.Freeze();
+    });
 
-        this.Restart();
-    }
+    ui.RandomChangeInput(config.maxKeyPress);
+    ui.DisplayKey();
+    ui.DisplayKeySpeed(config.keySpeedInterval, this.container, this.timer);
+    ui.HandleUserInput(
+      this.container,
+      this.timer,
+      config.increaseAmount,
+      config.decreaseAmount
+    );
 
-    StartGame() {
-        if (this.started) {
-            return;
-        }
+    this.started = true;
+  }
 
-        this.timer.ResetTimer(config.startTime);
-        this.timer.StartTimer();
-        this.timer.OnTimeUpEvent(() => {
-            ui.DisplayLossDialog(this);
-            this.container.Freeze();
-        });
-
-        ui.RandomChangeInput(config.maxKeyPress);
-        ui.DisplayKey();
-        ui.DisplayKeySpeed(config.keySpeedInterval, this.container, this.timer);
-        ui.HandleUserInput(this.container, this.timer, config.increaseAmount, config.decreaseAmount);
-
-        this.started = true;
-    }
-
-    Restart() {
-        this.started = false;
-        this.container.Initialize(config.containerInitializeValue);
-        this.timer.ResetTimer(config.startTime);
-        window.addEventListener('keyup', this.StartGame.bind(this));
-    }
-
+  Restart() {
+    this.started = false;
+    this.container.Initialize(config.containerInitializeValue);
+    this.timer.ResetTimer(config.startTime);
+    window.addEventListener("keydown", this.StartGame.bind(this));
+  }
 }
