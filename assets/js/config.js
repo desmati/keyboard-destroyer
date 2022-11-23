211ca7fe25f59a67a01d275e876c8ae4b5995b9f
desmati <desmati@live.com>
2022-11-23 10:46:58 +0100

    audio player

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,10 +2,10 @@ const config = {
     containerInitializeValue: 30,
     startTime: 30,
     keySpeedInterval: 500,
-    increaseAmount: 30,
+    increaseAmount: 5,
     decreaseAmount: 0,
     maxKeyPress: 1,
-    scoreboardCount:5,
+    scoreboardCount: 5,
     db: {
         apiKey: "AIzaSyBVbu5MI6HJfMIp5R0dMS7b2QW6VpIGWsA",
         authDomain: "desmati-30ce9.firebaseapp.com",
@@ -15,5 +15,12 @@ const config = {
         messagingSenderId: "623141315290",
         appId: "1:623141315290:web:2de9de5544ecb6191e7be3",
         measurementId: "G-SBKECL9WBP"
+    },
+    media: {
+        hover: './assets/media/hover.mp3',
+        click: './assets/media/click.mp3',
+        meow: './assets/media/meow.mp3',
+        blink: './assets/media/blink.mp3',
+        eating: ['./assets/media/eating1.mp3', './assets/media/eating2.mp3', './assets/media/eating3.mp3'],
     }
 };
